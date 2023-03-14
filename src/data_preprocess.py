fa48afb012bf7a986ab9b1a6fd2bd695f71505ed
Teslim <olayiwolateslim9@gmail.com>
2023-03-13 21:30:56 -0500

    implement dimensionality reductions

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -9,13 +9,17 @@ class DataPreprocessing():
 
         return data
     
-    def load_data(self, input_path, target_path, is_dataframe):
+    def load_data(self, input_path, target_path = None, is_dataframe = True, justFeatures = True):
+        
         input = self.read_data(input_path)
-        target = self.read_data(target_path)
 
-        self.data = pd.concat([input, target], axis=1)
+        if (justFeatures == True )or (target_path == None): # returns only the features w/o label
+            self.data = input
+        else:
+            target = self.read_data(target_path)
+            self.data = pd.concat([input, target], axis=1)
 
-        if is_dataframe == True:
+        if is_dataframe == True:  # returns a dataframe
             self.data = self.data
         else:
             self.data = self.data.to_numpy()
